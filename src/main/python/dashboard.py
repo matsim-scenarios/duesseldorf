ec6a957ae807eaf35272a60075bd4b29aee7b063
bolga <dziakowski@vsp.tu-berlin.de>
2022-03-08 21:41:48 +0100

    add current changes

--- src/main/python/dashboard.py
+++ src/main/python/dashboard.py
@@ -7,6 +7,9 @@ import pandas as pd
 import geopandas as gpd
 import matsim
 import simwrapper
+import seaborn as sns
+import matplotlib.pyplot as plt
+
 
 out = "../simwrapper"
 
@@ -179,42 +182,87 @@ def dist(df):
 
 #%%
 
-base = read_trips("base", in_city_region)
+base = read_trips("scenario-base", in_city_region)
+base_nmc = read_trips("scenario-base--no-mc", in_city_region)
 
 #%%%
 
-acv100 = read_trips("CV-0_ACV-100_AV-0", in_city_region)
-acv25 = read_trips("CV-75_ACV-25_AV-0", in_city_region)
-acv50 = read_trips("CV-50_ACV-50_AV-0", in_city_region)
-acv75 = read_trips("CV-25_ACV-75_AV-0", in_city_region)
+sc_st = read_trips("scenario-st", in_city_region)
+sc_mt = read_trips("scenario-mt", in_city_region)
+sc_lt = read_trips("scenario-lt", in_city_region)
 
 #%%
 
-acv50_st = read_trips("CV-50_ACV-50_AV-0--no-mc", in_city_region)
-acv100_st = read_trips("CV-0_ACV-100_AV-0--no-mc", in_city_region)
+sc_st_nmc = read_trips("scenario-st--no-mc", in_city_region)
+sc_mt_nmc = read_trips("scenario-mt--no-mc", in_city_region)
+sc_lt_nmc = read_trips("scenario-lt--no-mc", in_city_region)
 
 #%%
 
-shift_50 = pd.merge(base, acv50, on="trip_id")
+def norm(x):
+    return round((x - 1) * 1000) / 10
+
+def cmp(base, ref):
+    b = base[base.main_mode == "car"]
+    r = ref[ref.main_mode == "car"]
+    
+    print("trips", norm(len(r) / len(b)), "%")
+    print("dist", norm(dist(r) / dist(b)), "%")
+    print("tt",  norm(tt(r) / tt(b)), "%")
+
+
+#%%
+
+def netto_shift(series):
+    # Calculate the netto mode-choice shift by substracting differences    
+    for idx, count in series.iteritems():
+        
+        if idx[0] == idx[1]:
+            continue
+                
+        r_idx = (idx[1], idx[0])
+        
+        reverse = df.loc[r_idx]
+        
+        if reverse < count:
+            series.loc[r_idx] = 0
+            series.loc[idx] = count - reverse
+            
+        else:            
+            series.loc[r_idx] = reverse - count
+            series.loc[idx] = 0        
+        
+        
+    return series
+
+
+shift_50 = pd.merge(base, sc_mt, on="trip_id")
 df = shift_50.groupby(["main_mode_x", "main_mode_y"]).size()
-df.to_csv(join(out, "shift_acv50.csv"))
+df = netto_shift(df)
 
-shift_100 = pd.merge(base, acv100, on="trip_id")
+df.to_csv(join(out, "shift_mt.csv"))
+
+shift_100 = pd.merge(base, sc_lt, on="trip_id")
 df = shift_100.groupby(["main_mode_x", "main_mode_y"]).size()
-df.to_csv(join(out, "shift_acv100.csv"))
+df = netto_shift(df)
+
+df.to_csv(join(out, "shift_lt.csv"))
 
 #%%
 
-c0 = pd.read_csv("Z:/matsim-duesseldorf/experiment/output/base/congestion.csv.gz").set_index("link_id")
-c100 = pd.read_csv("Z:/matsim-duesseldorf/experiment/output/CV-50_ACV-50_AV-0--no-mc/congestion.csv.gz").set_index("link_id")
-c50 = pd.read_csv("Z:/matsim-duesseldorf/experiment/output/CV-0_ACV-100_AV-0--no-mc/congestion.csv.gz").set_index("link_id")
+c_base = pd.read_csv("Z:/matsim-duesseldorf/experiment/output/scenario-base--no-mc/congestion.csv.gz").set_index("link_id")
+c_st = pd.read_csv("Z:/matsim-duesseldorf/experiment/output/scenario-st--no-mc/congestion.csv.gz").set_index("link_id")
+c_mt = pd.read_csv("Z:/matsim-duesseldorf/experiment/output/scenario-mt--no-mc/congestion.csv.gz").set_index("link_id")
+c_lt = pd.read_csv("Z:/matsim-duesseldorf/experiment/output/scenario-lt--no-mc/congestion.csv.gz").set_index("link_id")
 
-ci0 = city_links.merge(c0, left_on="link_id", right_index=True).drop(columns=['index_right', 'Quelle', 'Stand', 'congestion_index', 'average_daily_speed', 'wkt'])
+ci0 = city_links.merge(c_base, left_on="link_id", right_index=True).drop(columns=['index_right', 'Quelle', 'Stand', 'congestion_index', 'average_daily_speed', 'wkt'])
 
 # Only look at links that are at least slightly congested
 ci0 = ci0[ci0.mean(axis=1) != 1]
 
 #%%
+
+# TODO: relative travel time für long-term (Mit MC)
 
 def convertSeconds(seconds):
     h = seconds//(60*60)
@@ -230,14 +278,15 @@ def ci_index(df, name):
     
         ds.append({
             "scenario": name,
-            "t": int(t),
+            "t": pd.to_datetime(t, unit='s'),
             "y": y
         })
     return ds
 
 
-ci100 = c100.loc[ci0.index].drop(columns=['congestion_index', 'average_daily_speed'])
-ci50 = c50.loc[ci0.index].drop(columns=['congestion_index', 'average_daily_speed'])
+ci_st = c_st.loc[ci0.index].drop(columns=['congestion_index', 'average_daily_speed'])
+ci_mt = c_mt.loc[ci0.index].drop(columns=['congestion_index', 'average_daily_speed'])
+ci_lt = c_lt.loc[ci0.index].drop(columns=['congestion_index', 'average_daily_speed'])
 
 
 #%%
@@ -245,10 +294,87 @@ ci50 = c50.loc[ci0.index].drop(columns=['congestion_index', 'average_daily_speed
 ds = []
 
 ds.extend(ci_index(ci0, "Base"))
-ds.extend(ci_index(ci50, "50% ACV"))
-ds.extend(ci_index(ci100, "100% ACV"))
+ds.extend(ci_index(ci_st, "Near future"))
+ds.extend(ci_index(ci_mt, "Mid term"))
+ds.extend(ci_index(ci_lt, "Distant future"))
 
 df = pd.DataFrame(ds)
 
-df.to_csv(join(out, "rel_tt.csv"), index=False)
+df.to_csv(join(out, "scenarios", "rel_tt.csv"), index=False)
 
+
+#%%
+
+# enum ("s" = straight, "t" = turn, "l" = left, "r" = right, "L" = partially left, R = partially right, "invalid" = no direction)
+
+def direction(x):
+    if x == "s":
+        return "straight"
+    elif x in ("L", "l"):
+        return "left"    
+    elif x in ("R", "r"):
+        return "right"
+    
+    return "turn"
+
+lanes = pd.read_csv("lanes.csv.gz").groupby(["fromEdgeId", "toEdgeId"]).first()
+lanes["direction"] = lanes.dir.apply(direction)
+lanes = lanes[["direction", "numLanes"]]
+
+
+base = pd.read_csv("scenario-base.csv")
+
+# Low flows are filtered out, because these are often network defects
+base = base[base.flow > 350]
+
+base = base.rename(columns={"flow": "base"})
+
+#%%
+
+df = pd.merge(base, lanes, left_on=["fromEdgeId", "toEdgeId"], right_index=True)
+
+for sc in ("st", "mt", "lt"):
+    
+    tmp = pd.read_csv("scenario-%s.csv" % sc)
+    tmp = tmp.drop(columns={"junctionId"})
+        
+    df = pd.merge(df, tmp, left_on=["fromEdgeId", "toEdgeId"], right_on=["fromEdgeId", "toEdgeId"])
+    
+    df[sc] = df.flow / df.base
+    df = df.drop(columns=["flow"])
+    
+    
+    
+df = df.drop(columns={"junctionId", "fromEdgeId", "toEdgeId"})
+    
+df = pd.melt(df, id_vars=["direction"], value_vars=["st", "mt", "lt"], value_name="change",  var_name="scenario")
+
+limits = {s: (float(df[df.scenario==s].quantile(0.02)), float(df[df.scenario==s].quantile(0.98))) for s in set(df.scenario)}
+
+# Remove few outliers
+def outlier(x):
+    l, h = limits[x.scenario]    
+    return l <= x.change <= h
+
+df = df[df.apply(outlier, axis=1)]
+
+df.to_csv(join(out, "flow", "intersections.csv"), index=False)
+
+#%%
+
+sns.set_theme(style="whitegrid", context="paper")
+
+df.loc[df.direction != "straight", "direction"] = "turn"
+
+df.loc[df.scenario == "st", "scenario"] = "Near future"
+df.loc[df.scenario == "mt", "scenario"] = "Mid-term"
+df.loc[df.scenario == "lt", "scenario"] = "Distant future"
+
+
+fig, ax = plt.subplots(dpi=350, figsize=(9, 3.2))
+
+ax = sns.violinplot(y="scenario", x="change", hue="direction", data=df, split=True, ax=ax)
+
+plt.xlim(0.8, None)
+plt.xlabel("Relative flow capacity")
+plt.ylabel("Scenario")
